a0136f701d4f131fe18c044eea34081f9f2db574
Vasiliev_AA <alexvas1984@yandex.ru>
2015-07-14 13:50:44 +0300

    Add blank activity

---
+++ app/src/main/java/com/beldolina/agentonline/MainActivity.java
@@ -0,0 +1,16 @@
+package com.beldolina.agentonline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vasiliev_AA on 14.07.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
